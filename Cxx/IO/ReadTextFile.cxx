e66acfcc92f0d6d724c69c9bdf04076819d812b5
perry <perry2163@gmail.com>
2017-11-15 23:40:58 -0800

    read TPLAN bone triangle data plus 4 registration band points

--- Cxx/IO/ReadTextFile.cxx
+++ Cxx/IO/ReadTextFile.cxx
@@ -1,17 +1,20 @@
 #include <vtkVersion.h>
 #include <vtkSmartPointer.h>
 #include <vtkPoints.h>
-#include <vtkVertexGlyphFilter.h>
 #include <vtkPolyDataMapper.h>
 #include <vtkActor.h>
 #include <vtkRenderer.h>
 #include <vtkRenderWindow.h>
 #include <vtkRenderWindowInteractor.h>
+#include <vtkTriangle.h>
+#include <vtkCellArray.h>
+#include <vtkPolyData.h>
 
 #include <sstream>
 
 int main(int argc, char* argv[])
 {
+  #if 0
   // Verify input arguments
   if ( argc != 2 )
   {
@@ -21,43 +24,82 @@ int main(int argc, char* argv[])
   }
   // Get all data from the file
   std::string filename = argv[1];
+  #endif 
+
+  std::string filename = "D:\\VTKTest\\THA-210-Trans-CD\\patient\\TSI50001_stem.PTL";
+  //std::string filename = "D:\\VTKTest\\THA-210-Trans-CD\\patient\\TSI50001_cup.PTL";
   std::ifstream filestream(filename.c_str());
 
+  //skip the header info part
   std::string line;
-  vtkSmartPointer<vtkPoints> points =
-    vtkSmartPointer<vtkPoints>::New();
+  while (std::getline(filestream, line)) {
+	  if (line[0] == '*') break;
+  }
+  
+  //skip the dummy line
+  std::getline(filestream, line);
 
-  while(std::getline(filestream, line))
+  //read total points number
+  std::getline(filestream, line);
+  double total_points, total_facets, total_vertices_on_facets;
+  std::stringstream lineInfo;
+  lineInfo << line;
+  lineInfo >> total_points >> total_facets >> total_vertices_on_facets;
+
+  cout << "total_points=[" << total_points << "], total_facets =[" << total_facets << "], total_vertices_on_facets=[" << total_vertices_on_facets << "]" << endl;
+  vtkSmartPointer<vtkPoints> points =
+    vtkSmartPointer<vtkPoints>::New();
+  double pointCnt = 0;
+
+  while(pointCnt < total_points && std::getline(filestream, line))
   {
+	pointCnt++;
     double x, y, z;
     std::stringstream linestream;
     linestream << line;
     linestream >> x >> y >> z;
 
     points->InsertNextPoint(x, y, z);
+    //cout << "[" << x << "," << y << "," << z << "]" << endl;
+  }
+
+  //read triangle data
+  vtkSmartPointer<vtkCellArray> triangles = vtkSmartPointer<vtkCellArray>::New();
+  vtkSmartPointer<vtkTriangle> triangle   =
+    vtkSmartPointer<vtkTriangle>::New();
+  double facetCnt = 0;
+  while (facetCnt < total_facets && std::getline(filestream, line))
+  {
+	  facetCnt++;
+	  int vertexCount, Vertex1, Vertex2, Vertex3, Color;
+	  std::stringstream linestream;
+	  linestream << line;
+	  linestream >> vertexCount >> Vertex1 >> Vertex2 >> Vertex3 >> Color;
+
+	  triangle->GetPointIds()->SetId(0, Vertex1);
+	  triangle->GetPointIds()->SetId(1, Vertex2);
+	  triangle->GetPointIds()->SetId(2, Vertex3);
+	  triangles->InsertNextCell(triangle);
   }
 
+  //close the file stream
   filestream.close();
+
+  // Create a polydata object
+  vtkSmartPointer<vtkPolyData> polyData =
+    vtkSmartPointer<vtkPolyData>::New();
+
+  // Add the geometry and topology to the polydata
+  polyData->SetPoints(points);
+  polyData->SetPolys(triangles);
+
+  std::cout << "There are " << polyData->GetNumberOfCells() << " cells." << std::endl;
 
-  vtkSmartPointer<vtkPolyData> polyData =
-    vtkSmartPointer<vtkPolyData>::New();
-
-  polyData->SetPoints(points);
-
-  vtkSmartPointer<vtkVertexGlyphFilter> glyphFilter =
-    vtkSmartPointer<vtkVertexGlyphFilter>::New();
-#if VTK_MAJOR_VERSION <= 5
-  glyphFilter->SetInputConnection(polyData->GetProducerPort());
-#else
-  glyphFilter->SetInputData(polyData);
-#endif
-  glyphFilter->Update();
-
-  // Visualize
-
+  // Visualize
+
   vtkSmartPointer<vtkPolyDataMapper> mapper =
     vtkSmartPointer<vtkPolyDataMapper>::New();
-  mapper->SetInputConnection(glyphFilter->GetOutputPort());
+  mapper->SetInputData(polyData);
 
   vtkSmartPointer<vtkActor> actor =
     vtkSmartPointer<vtkActor>::New();
