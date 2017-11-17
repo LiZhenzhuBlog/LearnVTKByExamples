03910ed3eb14233d4a391659a6fd202e5820baa0
Perry Zhang <perry2163@gmail.com>
2017-11-16 17:31:49 -0800

    Add axis into visualization to help understand the main object.

--- Cxx/Filtering/IterativeClosestPointsTransform.cxx
+++ Cxx/Filtering/IterativeClosestPointsTransform.cxx
@@ -18,230 +18,240 @@
 #include <vtkRenderWindowInteractor.h>
 #include <vtkXMLPolyDataReader.h>
 #include <vtkProperty.h>
+#include <vtkAxesActor.h>
 
-namespace
-{
-void CreatePolyData(vtkSmartPointer<vtkPolyData> polydata);
-void PerturbPolyData(vtkSmartPointer<vtkPolyData> polydata);
-void TranslatePolyData(vtkSmartPointer<vtkPolyData> polydata);
+namespace {
+    void CreatePolyData(vtkSmartPointer<vtkPolyData> polydata);
+    void PerturbPolyData(vtkSmartPointer<vtkPolyData> polydata);
+    void TranslatePolyData(vtkSmartPointer<vtkPolyData> polydata);
 }
 
 int main(int argc, char *argv[])
 {
-  vtkSmartPointer<vtkPolyData> source =
+    vtkSmartPointer<vtkPolyData> source =
     vtkSmartPointer<vtkPolyData>::New();
-  vtkSmartPointer<vtkPolyData> target =
+    vtkSmartPointer<vtkPolyData> target =
     vtkSmartPointer<vtkPolyData>::New();
-    
-  if(argc == 3)
-    {
-    std::cout << "Reading data..." << std::endl;
-    std::string strSource = argv[1];
-    std::string strTarget = argv[2];
-    vtkSmartPointer<vtkXMLPolyDataReader> sourceReader =
-      vtkSmartPointer<vtkXMLPolyDataReader>::New();
-    sourceReader->SetFileName(strSource.c_str());
-    sourceReader->Update();
-    source->ShallowCopy(sourceReader->GetOutput());
 
-    vtkSmartPointer<vtkXMLPolyDataReader> targetReader =
-      vtkSmartPointer<vtkXMLPolyDataReader>::New();
-    targetReader->SetFileName(strTarget.c_str());
-    targetReader->Update();
-    target->ShallowCopy(targetReader->GetOutput());
+    if (argc == 3) {
+        std::cout << "Reading data..." << std::endl;
+        std::string strSource = argv[1];
+        std::string strTarget = argv[2];
+        vtkSmartPointer<vtkXMLPolyDataReader> sourceReader =
+        vtkSmartPointer<vtkXMLPolyDataReader>::New();
+        sourceReader->SetFileName(strSource.c_str());
+        sourceReader->Update();
+        source->ShallowCopy(sourceReader->GetOutput());
+
+        vtkSmartPointer<vtkXMLPolyDataReader> targetReader =
+        vtkSmartPointer<vtkXMLPolyDataReader>::New();
+        targetReader->SetFileName(strTarget.c_str());
+        targetReader->Update();
+        target->ShallowCopy(targetReader->GetOutput());
+    } else {
+        std::cout << "Creating data..." << std::endl;
+        CreatePolyData(source);
+        target->ShallowCopy(source);
+        TranslatePolyData(target);
+        PerturbPolyData(target);
     }
-  else
-    {
-    std::cout << "Creating data..." << std::endl;
-    CreatePolyData(source);
-    target->ShallowCopy(source);
-    TranslatePolyData(target);
-    PerturbPolyData(target);
-    }
-  
-  // Setup ICP transform
-  vtkSmartPointer<vtkIterativeClosestPointTransform> icp = 
-      vtkSmartPointer<vtkIterativeClosestPointTransform>::New();
-  icp->SetSource(source);
-  icp->SetTarget(target);
-  icp->GetLandmarkTransform()->SetModeToRigidBody();
-  icp->SetMaximumNumberOfIterations(20);
-  //icp->StartByMatchingCentroidsOn();
-  icp->Modified();
-  icp->Update();
-  
-  // Get the resulting transformation matrix (this matrix takes the source points to the target points)
-  vtkSmartPointer<vtkMatrix4x4> m = icp->GetMatrix();
-  std::cout << "The resulting matrix is: " << *m << std::endl;
-  
-  // Transform the source points by the ICP solution
-  vtkSmartPointer<vtkTransformPolyDataFilter> icpTransformFilter =
+
+    // Setup ICP transform
+    vtkSmartPointer<vtkIterativeClosestPointTransform> icp = 
+    vtkSmartPointer<vtkIterativeClosestPointTransform>::New();
+    icp->SetSource(source);
+    icp->SetTarget(target);
+    icp->GetLandmarkTransform()->SetModeToRigidBody();
+    icp->SetMaximumNumberOfIterations(20);
+    //icp->StartByMatchingCentroidsOn();
+    icp->Modified();
+    icp->Update();
+
+    // Get the resulting transformation matrix (this matrix takes the source points to the target points)
+    vtkSmartPointer<vtkMatrix4x4> m = icp->GetMatrix();
+    std::cout << "The resulting matrix is: " << *m << std::endl;
+
+    // Transform the source points by the ICP solution
+    vtkSmartPointer<vtkTransformPolyDataFilter> icpTransformFilter =
     vtkSmartPointer<vtkTransformPolyDataFilter>::New();
-#if VTK_MAJOR_VERSION <= 5
-  icpTransformFilter->SetInput(source);
-#else
-  icpTransformFilter->SetInputData(source);
-#endif
-  icpTransformFilter->SetTransform(icp);
-  icpTransformFilter->Update();
+    icpTransformFilter->SetInputData(source);
+    icpTransformFilter->SetTransform(icp);
+    icpTransformFilter->Update();
 
-  /*
-  // If you need to take the target points to the source points, the matrix is:
-  icp->Inverse();
-  vtkSmartPointer<vtkMatrix4x4> minv = icp->GetMatrix();
-  std::cout << "The resulting inverse matrix is: " << *minv << std::cout;
-  */
-  
-  // Visualize
-  vtkSmartPointer<vtkPolyDataMapper> sourceMapper =
+    /*
+    // If you need to take the target points to the source points, the matrix is:
+    icp->Inverse();
+    vtkSmartPointer<vtkMatrix4x4> minv = icp->GetMatrix();
+    std::cout << "The resulting inverse matrix is: " << *minv << std::cout;
+    */
+
+    // Visualize
+    vtkSmartPointer<vtkPolyDataMapper> sourceMapper =
     vtkSmartPointer<vtkPolyDataMapper>::New();
-#if VTK_MAJOR_VERSION <= 5
-  sourceMapper->SetInputConnection(source->GetProducerPort());
-#else
-  sourceMapper->SetInputData(source);
-#endif
+    sourceMapper->SetInputData(source);
 
-  vtkSmartPointer<vtkActor> sourceActor =
+    vtkSmartPointer<vtkActor> sourceActor =
     vtkSmartPointer<vtkActor>::New();
-  sourceActor->SetMapper(sourceMapper);
-  sourceActor->GetProperty()->SetColor(1,0,0);
-  sourceActor->GetProperty()->SetPointSize(4);
+    sourceActor->SetMapper(sourceMapper);
+    sourceActor->GetProperty()->SetColor(0,1,0);//source is green
+    sourceActor->GetProperty()->SetPointSize(4);
 
-  vtkSmartPointer<vtkPolyDataMapper> targetMapper =
+    vtkSmartPointer<vtkPolyDataMapper> targetMapper =
     vtkSmartPointer<vtkPolyDataMapper>::New();
-#if VTK_MAJOR_VERSION <= 5
-  targetMapper->SetInputConnection(target->GetProducerPort());
-#else
-  targetMapper->SetInputData(target);
-#endif
-  
-  vtkSmartPointer<vtkActor> targetActor =
-    vtkSmartPointer<vtkActor>::New();
-  targetActor->SetMapper(targetMapper);
-  targetActor->GetProperty()->SetColor(0,1,0);
-  targetActor->GetProperty()->SetPointSize(4);
-  
-  vtkSmartPointer<vtkPolyDataMapper> solutionMapper =
-    vtkSmartPointer<vtkPolyDataMapper>::New();
-  solutionMapper->SetInputConnection(icpTransformFilter->GetOutputPort());
+    targetMapper->SetInputData(target);
 
-  vtkSmartPointer<vtkActor> solutionActor =
+    vtkSmartPointer<vtkActor> targetActor =
     vtkSmartPointer<vtkActor>::New();
-  solutionActor->SetMapper(solutionMapper);
-  solutionActor->GetProperty()->SetColor(0,0,1);
-  solutionActor->GetProperty()->SetPointSize(3);
-  
-  // Create a renderer, render window, and interactor
-  vtkSmartPointer<vtkRenderer> renderer =
+    targetActor->SetMapper(targetMapper);
+    targetActor->GetProperty()->SetColor(1,0,0); //target is red
+    targetActor->GetProperty()->SetPointSize(4);
+
+    vtkSmartPointer<vtkPolyDataMapper> solutionMapper =
+    vtkSmartPointer<vtkPolyDataMapper>::New();
+    solutionMapper->SetInputConnection(icpTransformFilter->GetOutputPort());
+
+    vtkSmartPointer<vtkActor> solutionActor =
+    vtkSmartPointer<vtkActor>::New();
+    solutionActor->SetMapper(solutionMapper);
+    solutionActor->GetProperty()->SetColor(0,0,1); //solution is blue
+    solutionActor->GetProperty()->SetPointSize(3);
+
+    // Create a renderer, render window, and interactor
+    vtkSmartPointer<vtkRenderer> renderer =
     vtkSmartPointer<vtkRenderer>::New();
-  vtkSmartPointer<vtkRenderWindow> renderWindow =
+    vtkSmartPointer<vtkRenderWindow> renderWindow =
     vtkSmartPointer<vtkRenderWindow>::New();
-  renderWindow->AddRenderer(renderer);
-  vtkSmartPointer<vtkRenderWindowInteractor> renderWindowInteractor =
+    renderWindow->AddRenderer(renderer);
+    renderWindow->SetSize( 1600, 968 );
+
+    vtkSmartPointer<vtkRenderWindowInteractor> renderWindowInteractor =
     vtkSmartPointer<vtkRenderWindowInteractor>::New();
-  renderWindowInteractor->SetRenderWindow(renderWindow);
+    renderWindowInteractor->SetRenderWindow(renderWindow);
 
-  // Add the actor to the scene
-  renderer->AddActor(sourceActor);
-  renderer->AddActor(targetActor);
-  renderer->AddActor(solutionActor);
-  renderer->SetBackground(.3, .6, .3); // Background color green
+    // Add the actor to the scene
+    renderer->AddActor(sourceActor);
+    renderer->AddActor(targetActor);
+    renderer->AddActor(solutionActor);
+    renderer->SetBackground(.3, .6, .3); // Background color green
 
-  // Render and interact
-  renderWindow->Render();
-  renderWindowInteractor->Start();
-  
-  return EXIT_SUCCESS;
+    //add axis
+    vtkSmartPointer<vtkTransform> transform = vtkSmartPointer<vtkTransform>::New();
+    transform->Translate(1.0, 1.0, 1.0);
+
+    vtkSmartPointer<vtkAxesActor> axes = vtkSmartPointer<vtkAxesActor>::New();
+
+    // The axes are positioned with a user transform
+    //axes->SetUserTransform(transform);
+    //axes->SetShaftTypeToLine();
+    // properties of the axes labels can be set as follows
+    // this sets the x axis label to red
+    axes->SetTotalLength(3.0,3.0,3.0);
+
+    #if 0
+    vtkTextProperty * xTxtProperty = axes->GetXAxisCaptionActor2D()->GetCaptionTextProperty();
+    vtkTextProperty * yTxtProperty = axes->GetYAxisCaptionActor2D()->GetCaptionTextProperty();
+    vtkTextProperty * zTxtProperty = axes->GetZAxisCaptionActor2D()->GetCaptionTextProperty();
+    xTxtProperty->SetColor(1,0,0);
+    yTxtProperty->SetColor(0,1,0);
+    zTxtProperty->SetColor(0,0,1);
+    xTxtProperty->SetFontSize(24); //why no effect at all?
+    yTxtProperty->SetFontSize(12);
+    zTxtProperty->SetFontFamilyToTimes(); //SetFontFamilyToCourier(); //SetFontFamilyToArial();
+    zTxtProperty->SetFontSize(8);
+    // the actual text of the axis label can be changed:
+    // axes->SetXAxisLabelText("test");
+    #endif
+
+    renderer->AddActor(axes);
+
+    // Render and interact
+    renderWindow->Render();
+    renderWindowInteractor->Start();
+
+    return EXIT_SUCCESS;
 }
 
-namespace // anonymous
-{
+namespace { // anonymous
 
-void CreatePolyData(vtkSmartPointer<vtkPolyData> polydata)
-{
-  // This function creates a set of 4 points (the origin and a point unit distance along each axis)
-  
-  vtkSmartPointer<vtkPoints> points =
-    vtkSmartPointer<vtkPoints>::New();
-  
-  // Create points
-  double origin[3] = {0.0, 0.0, 0.0};
-  points->InsertNextPoint(origin);
-  double p1[3] = {1.0, 0.0, 0.0};
-  points->InsertNextPoint(p1);
-  double p2[3] = {0.0, 1.0, 0.0};
-  points->InsertNextPoint(p2);
-  double p3[3] = {0.0, 0.0, 1.0};
-  points->InsertNextPoint(p3);
-    
-  vtkSmartPointer<vtkPolyData> temp = 
-    vtkSmartPointer<vtkPolyData>::New();
-  temp->SetPoints(points);
-  
-  vtkSmartPointer<vtkVertexGlyphFilter> vertexFilter = 
-    vtkSmartPointer<vtkVertexGlyphFilter>::New();
-#if VTK_MAJOR_VERSION <= 5
-  vertexFilter->SetInputConnection(temp->GetProducerPort());
-#else
-  vertexFilter->SetInputData(temp);
-#endif
-  vertexFilter->Update();
+    #if dataPreparation
+    source  (4 points = origin + unit distance on xyz axis) [green]
+    ====>
+    target =  source * [0, .3, 0] [red]
+    ===> add noise [0, 0.1] to target
+    source + target == ICP ==> tranformation Matrix
+    source + icp->matrix   ==> solution (should pretty close to target points)
+    #endif
 
-  polydata->ShallowCopy(vertexFilter->GetOutput());
-}
-
-void PerturbPolyData(vtkSmartPointer<vtkPolyData> polydata)
-{
-  vtkSmartPointer<vtkPoints> points =
-    vtkSmartPointer<vtkPoints>::New();
-  points->ShallowCopy(polydata->GetPoints());
-
-  for(vtkIdType i = 0; i < points->GetNumberOfPoints(); i++)
+    void CreatePolyData(vtkSmartPointer<vtkPolyData> polydata)
     {
-    double p[3];
-    points->GetPoint(i, p);
-    double perturb[3];
-    if(i%3 == 0)
-      {
-      perturb[0] = .1; perturb[1] = 0; perturb[2] = 0;
-      }
-    else if(i%3 == 1)
-      {
-      perturb[0] = 0; perturb[1] = .1; perturb[2] = 0;
-      }
-    else
-      {
-      perturb[0] = 0; perturb[1] = 0; perturb[2] = .1;
-      }
-      
-    for(unsigned int j = 0; j < 3; j++)
-      {
-      p[j] += perturb[j];
-      }
-    points->SetPoint(i, p);
+        // This function creates a set of 4 points (the origin and a point unit distance along each axis)
+
+        vtkSmartPointer<vtkPoints> points =
+        vtkSmartPointer<vtkPoints>::New();
+
+        // Create points
+        double origin[3] = {0.0, 0.0, 0.0};
+        points->InsertNextPoint(origin);
+        double p1[3] = {1.0, 0.0, 0.0};
+        points->InsertNextPoint(p1);
+        double p2[3] = {0.0, 1.0, 0.0};
+        points->InsertNextPoint(p2);
+        double p3[3] = {0.0, 0.0, 1.0};
+        points->InsertNextPoint(p3);
+
+        vtkSmartPointer<vtkPolyData> temp = 
+        vtkSmartPointer<vtkPolyData>::New();
+        temp->SetPoints(points);
+
+        //why need this filter. point != vertexFillter output?
+        vtkSmartPointer<vtkVertexGlyphFilter> vertexFilter = vtkSmartPointer<vtkVertexGlyphFilter>::New();
+        vertexFilter->SetInputData(temp);
+        vertexFilter->Update();
+
+        polydata->ShallowCopy(vertexFilter->GetOutput());
     }
 
-  polydata->SetPoints(points);
+    void PerturbPolyData(vtkSmartPointer<vtkPolyData> polydata)
+    {
+        vtkSmartPointer<vtkPoints> points =
+        vtkSmartPointer<vtkPoints>::New();
+        points->ShallowCopy(polydata->GetPoints());
 
-}
+        for (vtkIdType i = 0; i < points->GetNumberOfPoints(); i++) {
+            double p[3];
+            points->GetPoint(i, p);
+            double perturb[3];
+            if (i%3 == 0) {
+                perturb[0] = .1; perturb[1] = 0; perturb[2] = 0;
+            } else if (i%3 == 1) {
+                perturb[0] = 0; perturb[1] = .1; perturb[2] = 0;
+            } else {
+                perturb[0] = 0; perturb[1] = 0; perturb[2] = .1;
+            }
 
-void TranslatePolyData(vtkSmartPointer<vtkPolyData> polydata)
-{
-  vtkSmartPointer<vtkTransform> transform =
-    vtkSmartPointer<vtkTransform>::New();
-  transform->Translate(0,.3,0);
+            for (unsigned int j = 0; j < 3; j++) {
+                p[j] += perturb[j];
+            }
+            points->SetPoint(i, p);
+        }
 
-  vtkSmartPointer<vtkTransformPolyDataFilter> transformFilter =
-    vtkSmartPointer<vtkTransformPolyDataFilter>::New();
-#if VTK_MAJOR_VERSION <= 5
-  transformFilter->SetInputConnection(polydata->GetProducerPort());
-#else
-  transformFilter->SetInputData(polydata);
-#endif
-  transformFilter->SetTransform(transform);
-  transformFilter->Update();
-  
-  polydata->ShallowCopy(transformFilter->GetOutput());
-}
+        polydata->SetPoints(points);
+
+    }
+
+    void TranslatePolyData(vtkSmartPointer<vtkPolyData> polydata)
+    {
+        vtkSmartPointer<vtkTransform> transform =
+        vtkSmartPointer<vtkTransform>::New();
+        transform->Translate(0,.3,0);
+
+        vtkSmartPointer<vtkTransformPolyDataFilter> transformFilter =
+        vtkSmartPointer<vtkTransformPolyDataFilter>::New();
+        transformFilter->SetInputData(polydata);
+        transformFilter->SetTransform(transform);
+        transformFilter->Update();
+
+        polydata->ShallowCopy(transformFilter->GetOutput());
+    }
 
 } // end anonymous namespace
--- Cxx/GeometricObjects/Axes.cxx
+++ Cxx/GeometricObjects/Axes.cxx
@@ -4,27 +4,29 @@
 #include <vtkRenderer.h>
 #include <vtkRenderWindowInteractor.h>
 #include <vtkPolyData.h>
-#include <vtkSphereSource.h>
+#include <vtkCubeSource.h>
 #include <vtkAxesActor.h>
 #include <vtkTransform.h>
 #include <vtkSmartPointer.h>
+#include <vtkCaptionActor2D.h>
+#include <vtkTextProperty.h>
  
 int main (int, char*[])
 {
-  vtkSmartPointer<vtkSphereSource> sphereSource =
-    vtkSmartPointer<vtkSphereSource>::New();
-  sphereSource->SetCenter(0.0, 0.0, 0.0);
-  sphereSource->SetRadius(0.5);
+  vtkSmartPointer<vtkCubeSource> cubeSource =
+    vtkSmartPointer<vtkCubeSource>::New();
+  //cubeSource->SetCenter(0.0, 0.0, 0.0);
+  cubeSource->SetBounds(-1.0, 1.0,-1.0, 1.0,-1.0, 1.0);
 
   //create a mapper
-  vtkSmartPointer<vtkPolyDataMapper> sphereMapper =
+  vtkSmartPointer<vtkPolyDataMapper> cubeMapper =
     vtkSmartPointer<vtkPolyDataMapper>::New();
-  sphereMapper->SetInputConnection(sphereSource->GetOutputPort());
+  cubeMapper->SetInputConnection(cubeSource->GetOutputPort());
  
   // create an actor
-  vtkSmartPointer<vtkActor> sphereActor =
+  vtkSmartPointer<vtkActor> cubeActor =
     vtkSmartPointer<vtkActor>::New();
-  sphereActor->SetMapper(sphereMapper);
+  cubeActor->SetMapper(cubeMapper);
  
   // a renderer and render window
   vtkSmartPointer<vtkRenderer> renderer =
@@ -32,6 +34,7 @@ int main (int, char*[])
   vtkSmartPointer<vtkRenderWindow> renderWindow =
     vtkSmartPointer<vtkRenderWindow>::New();
   renderWindow->AddRenderer(renderer);
+  renderWindow->SetSize( 1600, 968 );
  
   // an interactor
   vtkSmartPointer<vtkRenderWindowInteractor> renderWindowInteractor =
@@ -39,23 +42,32 @@ int main (int, char*[])
   renderWindowInteractor->SetRenderWindow(renderWindow);
  
   // add the actors to the scene
-  renderer->AddActor(sphereActor);
+  renderer->AddActor(cubeActor);
   renderer->SetBackground(.1,.2,.3); // Background dark blue
  
   vtkSmartPointer<vtkTransform> transform =
     vtkSmartPointer<vtkTransform>::New();
-  transform->Translate(1.0, 0.0, 0.0);
+  transform->Translate(1.0, 1.0, 1.0);
 
-  vtkSmartPointer<vtkAxesActor> axes =
-    vtkSmartPointer<vtkAxesActor>::New();
+  vtkSmartPointer<vtkAxesActor> axes = vtkSmartPointer<vtkAxesActor>::New();
 
   // The axes are positioned with a user transform
-  axes->SetUserTransform(transform);
- 
+  //axes->SetUserTransform(transform);
+  //axes->SetShaftTypeToLine();
   // properties of the axes labels can be set as follows
   // this sets the x axis label to red
-  // axes->GetXAxisCaptionActor2D()->GetCaptionTextProperty()->SetColor(1,0,0);
+  axes->SetTotalLength(3.0,3.0,3.0);
 
+  vtkTextProperty * xTxtProperty = axes->GetXAxisCaptionActor2D()->GetCaptionTextProperty();
+  vtkTextProperty * yTxtProperty = axes->GetYAxisCaptionActor2D()->GetCaptionTextProperty();
+  vtkTextProperty * zTxtProperty = axes->GetZAxisCaptionActor2D()->GetCaptionTextProperty();
+  xTxtProperty->SetColor(1,0,0);
+  yTxtProperty->SetColor(0,1,0);
+  zTxtProperty->SetColor(0,0,1);
+  xTxtProperty->SetFontSize(24); //why no effect at all?
+  yTxtProperty->SetFontSize(12);
+  zTxtProperty->SetFontFamilyToTimes(); //SetFontFamilyToCourier(); //SetFontFamilyToArial();
+  zTxtProperty->SetFontSize(8);
   // the actual text of the axis label can be changed:
   // axes->SetXAxisLabelText("test");
 
@@ -63,7 +75,7 @@ int main (int, char*[])
  
   renderer->ResetCamera();
   renderWindow->Render();
- 
+
   // begin mouse interaction
   renderWindowInteractor->Start();
  
